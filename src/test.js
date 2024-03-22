9f309f27938878cf7cf068875d44eba48bb8c90a
eleonora <nebrudko18@gmail.com>
2024-03-22 13:07:47 +0000

    fix

--- src/test.js
+++
@@ -1,6 +0,0 @@
-function camelCase(str) {
-  const arr = str.split(' ')
-  arr.map((e, index) => (index === 0 ? e : e[index][0].toUpperCase() + e.slice(1)))
-  return arr.join('')
-}
-console.log(camelCase('wow hello'))
