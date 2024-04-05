5074e14f3f61ac6056c41a120cb4cf39b852c11d
eleonora <nebrudko18@gmail.com>
2024-04-05 13:17:41 +0100

    fix PaginationsCustom

--- src/components/PaginationsCustom/PaginationsCustom.tsx
+++ src/components/PaginationsCustom/PaginationsCustom.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react'
 import './index.scss'
-import { Box, Pagination, Stack, ThemeProvider, createTheme } from '@mui/material'
+import { Box, Pagination, Stack } from '@mui/material'
 
 type PaginationsCustomProps = {
   currentPage: number
@@ -9,24 +9,6 @@ type PaginationsCustomProps = {
 }
 
 export const PaginationsCustom: React.FC<PaginationsCustomProps> = ({ pagesCount, currentPage, setCurrentPage }) => {
-  console.log('currentPage', currentPage)
-  const theme = createTheme({
-    typography: {
-      fontFamily: 'Handlee, sans-serif',
-    },
-    components: {
-      MuiPaginationItem: {
-        styleOverrides: {
-          root: {
-            '&.Mui-selected': {
-              backgroundColor: '#c2c5cc',
-            },
-          },
-        },
-      },
-    },
-  })
-
   const setCurrentPageHandler = useCallback(
     (event: React.ChangeEvent<unknown>, page: number) => {
       setCurrentPage(page)
@@ -38,18 +20,16 @@ export const PaginationsCustom: React.FC<PaginationsCustomProps> = ({ pagesCount
 
   return (
     <Box className="pagination">
-      <ThemeProvider theme={theme}>
-        <Stack spacing={10}>
-          <Pagination
-            count={pagesCount}
-            size="small"
-            siblingCount={4}
-            shape="rounded"
-            page={currentPage}
-            onChange={setCurrentPageHandler}
-          />
-        </Stack>
-      </ThemeProvider>
+      <Stack spacing={10}>
+        <Pagination
+          count={pagesCount}
+          size="small"
+          siblingCount={4}
+          shape="rounded"
+          page={currentPage}
+          onChange={setCurrentPageHandler}
+        />
+      </Stack>
     </Box>
   )
 }
